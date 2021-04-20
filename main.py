97031f1f1c58324737dc822623e9a5754071566d
dohungnd <dohungnd94@gmail.com>
2021-04-20 15:05:26 +0700

    'update main'

--- main.py
+++ main.py
@@ -122,6 +122,6 @@ if __name__ == "__main__":
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run()
 # [END gae_python3_bigquery]
 # [END gae_python38_bigquery]
